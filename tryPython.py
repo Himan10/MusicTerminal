004a57f1d8e4b2d085fe14dc8b2a89cd9cd63d09
Himanshu Bhatnagar <33115688+Himan10@users.noreply.github.com>
2020-11-15 22:41:19 +0530

    Modified/Added existing/new methods
    Changes : 
    1. No need to create the `_play()` function as a thread function, because it will only execute once, and then we don't need it until the playlist is empty and user doesn't want to make a fresh start. 
    2. Replaced `sleep(5)` with `self._player.wait_until_playing()` in method `_play()`. This newer mpv.MPV() method blocks the execution of the function until the song doesn't start. It's actually better to block or say PAUSE the execution of a method using sleep(5) because `wait_until_playing()` knows how much time is necessary or needed to PAUSE the function instead of randomly submitting N for sleep().
    2. Added a `repeat()` method : Basically to repeat the song N times. Also, the user can provide some other parameters to this method like bool values, "inf" means infinite, "no" means normal etc. 
    3. Call `self.remove(0)` in `_track_song()` only if there is no repeat for any songs. One of the class attribute i.e., `self.repeat` handle this thing. If its value is equal to 0 then perform remove() operation otherwise keep playing the current song N times. 
    
    Haven't Added : 
    1. a prompt (single line)
    2. newer methods like "jump", "show queue" and others
    3. Save user playlist in either a txt or JSON object or sql db
    4. provide a config file for user
    5. Logging method

--- tryPython.py
+++ tryPython.py
@@ -1,15 +1,13 @@
 import mpv
 import threading
-from time import sleep
 import concurrent.futures
-from SongPathFinder import os
-from SongPathFinder import re
-from SongPathFinder import SearchSong
+from songPathFinder import os
+from songPathFinder import main
 
 exit = threading.Event()
 
-class MusicTerminal:
 
+class MusicTerminal:
     def __enter__(self):
         return self
 
@@ -17,80 +15,83 @@ class MusicTerminal:
         return isinstance(value, TypeError)
 
     def __init__(self, video: bool, ytdl=True, logging=False):
-        self._player = mpv.MPV() # returns a proxy object. (dont touch this)
-        self.playlist0 = [] # temp.
+        self._player = mpv.MPV()  # returns a proxy object. (dont touch this)
+        self.playlist0 = []  # temp. (saving)
         self._player.video = video
         self._player.ytdl = ytdl
-        self.playing_msg = 'stopped'
+        self.playing_msg = "stopped"
         self.logging = logging
         self.running = False
         self._threadExecutable = False
+        self.repeat = 0
 
-    def addsong(self, path: str, songs: list, executor): # Make this a thread
-        """ executor should be an obj of concurrent.futures.ThreadPoolExecutor """
+    def addsong(self, path: str, songs: list, executor):
+        """ executor should be an obj of 
+        concurrent.futures.ThreadPoolExecutor """
+
         songs = songs if isinstance(songs, list) else [songs]
         self.playlist0.extend(songs)
         for song in songs:
             if path is None:
                 self._player.playlist_append(song)
             else:
-                self._player.playlist_append(os.path.join(path , song))
-    
+                self._player.playlist_append(os.path.join(path, song))
+
         if self._player.playlist_count > 0:
             if self._threadExecutable:
                 pass
             else:
-                future1 = executor.submit(self._play) # Call it only once or if adding a song
-                return future1 # this future1.result() contains future of another thread
+                future1 = self._play()  # Call it only once or if adding a song
+                return future1 # contains result of another thread
 
     def _play(self):
         """ Shouldn't be executed in main
         State : [play, pause] -> BUSY
         State : [open, close] -> IDLE
         """
-        
         if self._player.idle_active:
             if self._player.playlist_count > 0:
                 if self.running is False:
                     self._player.playlist_pos = 0
+                    self._player.wait_until_playing() # PAUSE everything until song doesn't get started.
                     self._threadExecutable = True
-                    sleep(5) # Delay problem if sleep(1) not used here.
                     Future = executor.submit(self._track_song)
 
-                self.playing_msg = 'running'
+                self.playing_msg = "running"
                 self.running = not self._player.idle_active
                 print(self.running, self._player.idle_active)
-                print(f'\n == {self.playing_msg} ==\n')
+                print(f"\n == {self.playing_msg} ==\n")
             else:
-                self.playing_msg = 'stopped'
+                self.playing_msg = "stopped"
                 self.running = self._player.idle_active
 
         return Future
 
-    def _track_song(self): # Because we want real timestamp updates, not only once.
+    def _track_song(self):  # Because we want real timestamp updates, not only once.
 
-        while a:=self._player.playlist_count > 0:
-            song = filter(lambda x: 'playing' in x.keys(), self._player.playlist)
+        while self._player.playlist_count > 0:
+            song = filter(lambda x: "playing" in x.keys(), self._player.playlist)
             # check if the song is url or path
 
             if self._threadExecutable:
-                songName = os.path.basename(list(song)[0]['filename'])
-                #url = True if songName.startswith('https://www.youtube.com') else False
-                #delay = 4 if url else 0
-                print(songName)
+                songName = os.path.basename(list(song)[0]["filename"])
                 totaltime = self._player.time_pos + self._player.time_remaining
-                exit.wait(timeout=totaltime) #sleep(totaltime)
-                self.remove(0) # Delete the song from both queues
+                exit.wait(timeout=totaltime) 
+                if self.repeat > 0:
+                    self.repeat -= 1
+                if self.repeat == 0:
+                    self.remove(0, True)  # Delete the song from both queues
                 if len(self.playlist0) > 0:
-                    self._player.wait_until_playing()
+                    self._player.wait_until_playing() # wait until new song starts
 
-        print('here')
-        self.stop(False) # do not terminate
+        print("here")
+        self.stop(False)  # do not terminate
         return True
 
-    def remove(self, index: int):
+    def remove(self, index: int, orignal=False):
         if index < 0:
-            raise Exception('Negative Index not accepted')
+            raise Exception("Negative Index not accepted")
+        
         self.playlist0.pop(index)
         self._player.playlist_remove(index)
 
@@ -98,41 +99,62 @@ class MusicTerminal:
         # Not in idle state
         self._player.pause = val
         self.running = self._player.idle_active
-        self.playing_msg = 'paused'
+        self.playing_msg = "paused"
+
+    def repeat_song(self, others):
+        """ Repeat song : int(0-9) or str(0-9) """
+        # Put the current song in repeat (Under-working)
+        # no : normal playback
+        # inf : infinite
+        params = ['inf', True, False, 'no', 'yes']
+        if not (others in params):
+            if str(others) not in '0123456789':
+                raise Exception('Supplied wrong parameters')
+            else:
+                self.repeat += int(others)+1
+
+        self._player.loop_file = others
+        print(" current song on repeat")
 
     def resume(self, val=False):
         # Not in idle state
         self._player.pause = val
         self.running = not self._player.idle_active
-        self.playing_msg = 'running'
+        self.playing_msg = "running"
 
     def stop(self, terminate: bool):
         # put the player in IDLE state
-        self._player.stop()
+        self._player.stop() 
         self.running = False
-        self.playing_msg = 'stopped'
+        self.playing_msg = "stopped"
         self._threadExecutable = False
         if terminate:
-            self._player.terminate() # stop the thread. Destroys the mpv object
+            self._player.terminate()  # stop the thread. Destroys the mpv object
 
 
 if __name__ == "__main__":
 
-    choice = 'yes'
-    player = MusicTerminal(video=False)
+    musicObj = MusicTerminal(video=False)
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
-    path, songs = SearchSong()
-    future1 = player.addsong(path, songs, executor) # Might use partial function for this
-    sleep(6) #print('need sleep')
-    choice = ''
-    while player.running and choice != 'no': # Should accept more input
-        choice = input(' More [options|no|terminate|add] : ')
-        if choice == 'playlist':
-            print(player.playlist0)
-        if choice == 'terminate':
-            player.stop(True)
-        if choice == 'add':
-            p, s = SearchSong()
-            future2 = player.addsong(p, s, executor)
+    user_input = ''
+    while user_input != 'no':
+        user_input = input(f' [{musicObj.running}] [Add|Repeat|Terminate|Playlist] : ').lower()
+        if user_input == 'add':
+            path, song = main()
+            if song is None:
+                exit()
+            print('Song Added')
+            future1 = musicObj.addsong(path, song, executor)
 
-    # Once the choice = 'no', control of main goes to an end but befor that, all the threads running will complete their task.
+        if user_input == 'terminate':
+            musicObj.stop(True)
+            break
+
+        if user_input == 'repeat':
+            repeatParam = input(' Enter repeat param : ')
+            musicObj.repeat_song(repeatParam)
+
+        if user_input == 'playlist':
+            print(musicObj.playlist0)
+
+    print('exit')
