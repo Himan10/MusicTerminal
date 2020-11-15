e99ffe956c0165f784cd12a7ead3fa63b5ac8e45
Himanshu Bhatnagar <33115688+Himan10@users.noreply.github.com>
2020-11-15 22:22:08 +0530

    Shifted to OOP structure
    The only reason to shift my code from procedural structure to OOP because I wanted to wrap all the methods inside one class so that they can share/send things with each other especially in case of threading, and users only have to import this class, not separate functions.
    Although I haven't added any thread method but will keep it for future purposes. 
    
    Methods Added : 
    1. `LoadFromFile()`: This method loads the dictionary object stored in the `cache.txt` file (which basically contains youtube links associated with their title name -> 
    { 'crawling': <youtube_link> } and then to search that specific song name that user input, I added two different methods to get that song + link if it exists in the file.. so the first one is 
    IF DictObj[songname] is True, then return the link (value associated with the key "songname")
    ELSE, then perform a linear slice searching on every key of dictObj until it found.. I meant, suppose you've a string i.e., "crawling" and in that `cache.txt` file, you've a song named "crawlingonemorelight". Here, it checks in slices, means a pair of 3 characters from the string, IF all the slices (pair) of the user input string exist then return that `key` to the caller. 
    
    2. `YoutubeSearch`: This one is simple because if the song isn't found in local directory and not even in cache file, then only choice left is to get its link from the youtube. 
    In this method, I made a use of `youtube-dl` (kind of youtube downloader, but not only supports youtube :P shaddy shaddy too :) ) . Once the song information is extracted and we got the song ID, title, or whatever we are interested in, then save the {songname: <youtube link>} into `cache.txt`.. in case if user wants to play the same song and it again doesn't exist in local dir, then search through the keys in `cache.txt` because in the first run, when we did a youtube search and got the link, we stored it in `cache.txt` 
     
    Haven't Added : 
    1. Extracting genre (threading)
    2. A good prompt for the end user to intract with methods. 
    3. Might be this code is not time efficient yet (yes it isn't)

--- songPathFinder.py
+++ songPathFinder.py
@@ -1,57 +1,139 @@
-""" 
-Find songs : 
+"""
+Find songs:
     1. Find in a local dir
-    2. Scrape the youtube link
+    2. [Alt. LOOKING] Check in the local saved cache
+    3. [Alt. HARD LOOKING] Scrape it from youtube
 """
 
 import os
 import re
+import json
 import youtube_dl
 
-def SearchSong(path='/home/hi-man/Music/') -> list:
-    """ Search a song : 
-    1. Local directory [local]
-    2. Fetch a youtube link (if it isn't found in local dir.) [not-local]
-    """
 
-    def remove_punctuations(string: str) -> str:
-        # remove path
-        # regex pattern to get a filename -> ^/(?:[^/]*){4}([^/]*)
-        # But i will simply use os.path.basename() to get a filename
-        filename = os.path.basename(string)
-        filename = re.sub('[\s\W_]', '', filename) # \s = whitespace, \W = [^A-Za-z0-9]
-        return filename
+def _remove_punctuations(string: str) -> str:
+    # remove path
+    # regex pattern to get a filename -> ^/(?:[^/]*){4}([^/]*)
+    # But i will simply use os.path.basename() to get a filename
+    filename = os.path.basename(string)
+    # \s = whitespace, \w = [^A-Za-z0-9]
+    filename = re.sub(r'[\s\W_]', "", filename)
+    return filename.lower()
 
-    name = ''
-    while len(name) < 1:
-        name = remove_punctuations(input(' Enter song name : '))
-    
-    pattern = f'.*{name}.*(?:mp[3|4]|opus|webm|wav)'
-    songs = []
-    found = False
 
-    for path, direc, files in os.walk(path, topdown=False):
-        # no sorted files.
-        for i in range(0, len(files)): # O(N)
-            if re.search(pattern, remove_punctuations(files[i]), re.IGNORECASE):
-                songs.append(files[i]) # O(1)
-                found = True
-        
-        if found is True:
-            break # Don't search in sub-directories
-    
-    song_path = (path, songs) if found is True else (None, None)
+class SearchSong:
 
-    ### check for youtube search
-    if song_path != (None, None):
+    def __init__(self, path=None):
+        self.dirpath = "/home/hi-man/Music/"
+
+    def SearchLocal(self, name) -> tuple:
+        """ Search a song :  Local directory [path] """
+
+        name = _remove_punctuations(name)
+        pattern = f".*{name}.*(?:mp[3|4]|opus|webm|wav)"
+        songs = []
+        found = False
+
+        for path, direc, files in os.walk(self.dirpath, topdown=False):
+            for i in range(0, len(files)):  # O(N)
+                if re.search(
+                        pattern, _remove_punctuations(files[i]), re.IGNORECASE
+                ):
+                    songs.append(files[i])  # O(1)
+                    found = True
+
+            if found is True:
+                break  # Don't search in sub-directories
+
+        song_path = (path, songs) if found is True else (None, None)
         return song_path
 
-    # Do youtube search
-    ytdl_options = {
-            'default_search': 'auto',
-            'quiet': True
-    }
 
-    with youtube_dl.YoutubeDL(ytdl_options) as ydl:
-        info_dict = ydl.extract_info(name, download=False)
-        return None, info_dict['entries'][0]['webpage_url']
+    def LoadFromFile(self, songname: str):
+        """
+        After not found in local dir, Check if the songname exist in
+        cache file... RETURN link if exist.
+        """
+
+        def MatchSong(songname: str, target: str):
+            found = True
+            for i in range(0, len(songname), 4):
+                if songname[i: i + 4] not in target:
+                    found = ''
+                    break
+
+            return found
+
+        found = ''
+        if not os.path.exists("./cache.txt"):
+            os.system("touch cache.txt")  # execute the shell command
+
+        if os.path.getsize("./cache.txt") != 0:
+            # Perform a scan
+            with open("./cache.txt", "r") as file:
+                songdata = json.load(file)
+                songname = _remove_punctuations(songname)
+                try:
+                    return songdata[songname]
+                except KeyError:
+                    # Perform some more scan, not directly access
+                    for key in songdata.keys():
+                        found = MatchSong(songname, key)
+                        if found is True:
+                            found = songdata[key]
+                            return found
+
+        return found
+
+
+    def YoutubeSearch(self, songname: str):
+
+        ytdl_options = {
+            "default_search": "auto",
+            "quiet": True,
+            "extractaudio": True,
+            "audioformat": "mp3",
+            "format": "bestaudio/best[filesize<20M]",
+            "noplaylist": True,
+            "outtmpl": "%(id)s",
+        }
+
+        youtube_link = "https://www.youtube.com/watch?v="
+        with youtube_dl.YoutubeDL(ytdl_options) as ydl:
+            try:
+                info_dict = ydl.extract_info(songname, download=False)
+                videoID = info_dict["entries"][0]["id"]
+                youtube_link += videoID
+
+                data = {}
+                with open("./cache.txt", "r") as file:
+                    if os.path.getsize("./cache.txt") != 0:
+                        data = json.load(file)
+
+                data[_remove_punctuations(songname)] = youtube_link
+
+                with open("./cache.txt", "w") as file:
+                    json.dump(data, file, indent=4)
+
+            except Exception as err:
+                print(err)
+
+        return youtube_link
+
+def main():
+    # a basic prompt for searching songs
+
+    searchObj = SearchSong()
+
+    songname = ''
+    while len(songname) < 1:
+        songname = input(' Enter song name : ')
+
+    found = searchObj.SearchLocal(songname)
+
+    if None in found:
+        found = searchObj.LoadFromFile(songname) # check in cache before youtube searching.
+        if found == '':
+            found = searchObj.YoutubeSearch(songname)
+
+    return found if isinstance(found, tuple) else (None, found)
