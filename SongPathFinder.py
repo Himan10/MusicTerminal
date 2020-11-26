0deff3ca172bed210bd3769d669ebad4b0aa9ac4
Himanshu Bhatnagar <33115688+Himan10@users.noreply.github.com>
2020-11-27 01:07:37 +0530

    Edited SearchLocal() method
    Added recursion technique to search for the songs in different directories, and if nothing is found in the first directory returned by yieldOrignalNames then it will make a second call to the generator function which returns another directory path.
    Now, the same computation will be done for this path too. if the song name is found then simply checks for each piece of user input that is split into pieces by using the .split() method, and once you get the final song name that contains all the words or at least contain more than half of the words of user input (split one) then it simply breaks the while loop, otherwise it again makes a call to a generator function, ask for another directory path and do the same.
    
    Problem: the function does the computation for the path that is already been checked.. (needs a memorization technique here)
    Fix: used a dictionary to store the result that we got from each directory... if a path exists in the dictionary then it simply returns the value (tuple) associated with that key (path) and doesn't do further computation for it.
    it now takes only 2 recursive calls to search a song (only case, if the song is not present in all the 3 directories) because I've only two sub-directories inside the main directory...
    If a song present in the first sub-directory then it will take only 1 recursive call = main(not found) -> sub-directory(found)
    If a song present in the main directory then no recursive call = main(found)
    
    Haven't fixed : 
    Exit or break condition if a song is found in any of the sub-directory, then when to do if we have True value stored in success? we don't need the control goes back to while loop and then make calls to the generator function.

--- SongPathFinder.py
+++ SongPathFinder.py
@@ -8,6 +8,7 @@ Find songs:
 import os
 import re
 import json
+import doctest
 import youtube_dl
 
 
@@ -15,41 +16,92 @@ def _remove_punctuations(string: str) -> str:
     # remove path
     # regex pattern to get a filename -> ^/(?:[^/]*){4}([^/]*)
     # But i will simply use os.path.basename() to get a filename
-    filename = os.path.basename(string)
+    #filename = os.path.basename(string)
     # \s = whitespace, \w = [^A-Za-z0-9]
-    filename = re.sub(r'[\s\W_]', "", filename)
+    filename = re.sub(r'[\s\W_]', "", string)
     return filename.lower()
 
 
 class SearchSong:
 
-    def __init__(self, path=None):
+    def __init__(self, songname: str, path=None):
         self.dirpath = "/home/hi-man/Music/"
+        self.songname  = songname
+        self.songname2 = _remove_punctuations(songname)
 
-    def SearchLocal(self, name) -> tuple:
-        """ Search a song :  Local directory [path] """
+    def _yieldOrignalNames(self, path: str):
+        """ call me again to get files stored in path """
 
-        name = _remove_punctuations(name)
-        pattern = f".*{name}.*(?:mp[3|4]|opus|webm|wav)"
+        for dir_, subdir, files in os.walk(path, topdown=True):
+            yield dir_, files
+
+    def SearchLocal(self):
+
+        def Helper(user_input: str, path: str, AlreadySearched: dict):
+            """ Search songs in local directories [Recursive] """
+
+            if path in AlreadySearched:
+                return AlreadySearched[path]
+
+            nonlocal orignalSongNames
+            genObj = self._yieldOrignalNames(path)
+            path, orignalSongNames = next(genObj)
+            # Pause the generator at yield statement
+            # until we send something to it or run next()
+
+            temp = ", ".join(
+                str(i) + "-" + _remove_punctuations(orignalSongNames[i])
+                for i in range(0, len(orignalSongNames))
+            )
+
+            # Find the songs
+            user_input = re.sub(r'[^\s\w]', ' ', user_input).split()
+            match = 0
+            n = len(user_input)
+            i = 0
+
+            while i < n and match < n:
+                pattern = r"[^,]*{0}[^,]*(?:mp[3|4]|opus|mkv|webm)".format(user_input[i])
+                found = re.findall(pattern, temp, re.I)  # returns a list
+                if found.__sizeof__() > 40:  # not an empty list
+                    i += 1
+                    match += 1
+                    temp = ", ".join(found)
+
+                    if match >= n:
+                        genObj.close()
+                        break
+                else:
+                    try:
+                        path, orignalSongNames = next(genObj) # Call for path of another dir.
+                        success, found, path = Helper(' '.join(user_input), path, AlreadySearched)
+                        AlreadySearched[path] = success, found, path
+                        if success:
+                            genObj.close()
+                            break
+                    except Exception as err:
+                        # Possible condition : We've iterated over all the paths but couldn't find any
+                        return False, None, path
+
+            AlreadySearched[path] = True, found, path
+            return True, found, path
+
+        # Main
+        orignalSongNames = []
+        AlreadySearched = {}
         songs = []
-        found = False
+        path = self.dirpath
+        result = Helper(self.songname, path, AlreadySearched)
+        del AlreadySearched
+        if result[0]:
+            for i in result[1]:
+                songs.append(orignalSongNames[int(i.lstrip().split('-')[0])])
+            return result[2], songs # Path, songs
 
-        for path, direc, files in os.walk(self.dirpath, topdown=False):
-            for i in range(0, len(files)):  # O(N)
-                if re.search(
-                        pattern, _remove_punctuations(files[i]), re.IGNORECASE
-                ):
-                    songs.append(files[i])  # O(1)
-                    found = True
-
-            if found is True:
-                break  # Don't search in sub-directories
-
-        song_path = (path, songs) if found is True else (None, None)
-        return song_path
+        return None, None
 
 
-    def LoadFromFile(self, songname: str):
+    def LoadFromFile(self):
         """
         After not found in local dir, Check if the songname exist in
         cache file... RETURN link if exist.
@@ -57,8 +109,8 @@ class SearchSong:
 
         def MatchSong(songname: str, target: str):
             found = True
-            for i in range(0, len(songname), 4):
-                if songname[i: i + 4] not in target:
+            for i in range(0, len(self.songname), 4):
+                if self.songname[i: i + 4] not in target:
                     found = ''
                     break
 
@@ -72,13 +124,12 @@ class SearchSong:
             # Perform a scan
             with open("./cache.txt", "r") as file:
                 songdata = json.load(file)
-                songname = _remove_punctuations(songname)
                 try:
-                    return songdata[songname]
+                    return songdata[self.songname2]
                 except KeyError:
                     # Perform some more scan, not directly access
                     for key in songdata.keys():
-                        found = MatchSong(songname, key)
+                        found = MatchSong(self.songname2, key)
                         if found is True:
                             found = songdata[key]
                             return found
@@ -86,7 +137,7 @@ class SearchSong:
         return found
 
 
-    def YoutubeSearch(self, songname: str):
+    def YoutubeSearch(self):
 
         ytdl_options = {
             "default_search": "auto",
@@ -101,7 +152,7 @@ class SearchSong:
         youtube_link = "https://www.youtube.com/watch?v="
         with youtube_dl.YoutubeDL(ytdl_options) as ydl:
             try:
-                info_dict = ydl.extract_info(songname, download=False)
+                info_dict = ydl.extract_info(self.songname, download=False)
                 videoID = info_dict["entries"][0]["id"] # song video ID
                 title = info_dict['entries'][0]['title'] # song video Title
                 youtube_link += videoID
@@ -111,7 +162,7 @@ class SearchSong:
                     if os.path.getsize("./cache.txt") != 0:
                         data = json.load(file)
 
-                data[_remove_punctuations(songname)] = youtube_link
+                data[self.songname2] = youtube_link
 
                 with open("./cache.txt", "w") as file:
                     json.dump(data, file, indent=4)
@@ -124,17 +175,16 @@ class SearchSong:
 def main():
     # a basic prompt for searching songs
 
-    searchObj = SearchSong()
-
     songname = ''
     while len(songname) < 1:
         songname = input(' Enter song name : ')
 
-    found = searchObj.SearchLocal(songname)
+    searchObj = SearchSong(songname)
+    found = searchObj.SearchLocal()
 
     if None in found:
-        found = searchObj.LoadFromFile(songname) # check in cache before youtube searching.
+        found = searchObj.LoadFromFile() # check in cache before youtube searching.
         if found == '':
-            found = searchObj.YoutubeSearch(songname)
+            found = searchObj.YoutubeSearch()
 
     return found if isinstance(found, tuple) else (None, found.split())
