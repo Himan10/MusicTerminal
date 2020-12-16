588ec668001652eaa0cb86a3998327fa0a3d0608
Himanshu Bhatnagar <33115688+Himan10@users.noreply.github.com>
2020-12-16 19:43:35 +0530

    altered some methods, especially stop()
    1. threading.Event as now a part of class `MusicTerminal` (self._threadStopper)
    2. Added functionality for `findmany()`
    3. replace all the `exit.<method>` with `self._threadStopper.<method>`
    4. shutdown the thread if the user wants to terminate the music terminal by passing bool value "True" to stop()

--- music_player.py
+++ music_player.py
@@ -1,14 +1,15 @@
 import mpv
-import threading # for threading event
+from threading import Event
+from threading import Thread
+from threading import enumerate
+from threading import current_thread
 from SongPathFinder import os
-from SongPathFinder import findSong
+from SongPathFinder import findone, findmany
 from connectToSql import SongDatabase
 from command_handler import Command
 from command_handler import InputParser
 from concurrent.futures import Executor # for executor type checking
 from concurrent.futures import ThreadPoolExecutor
-
-exit = threading.Event()
 
 
 class MusicTerminal(InputParser):
@@ -16,18 +17,20 @@ class MusicTerminal(InputParser):
         return self
 
     def __exit__(self, type, value, traceback):
-        self._player.terminate()
+        self.stop(True)
         return isinstance(value, TypeError)
 
     def __init__(self, video: bool, prefix: str, ytdl=True, logging=False, event_thread=True):
         super().__init__(prefix)
         self._player = mpv.MPV()  # returns a proxy object. (dont touch this)
         self.playlist0 = []  # temp. (provide to SQL)
+        self.playlistpos = 0 # orignal playlist position 
         self._player.video = video
         self._player.ytdl = ytdl
         self.playing_msg = "stopped"
         self.logging = logging
         self.running = False
+        self._threadStopper = Event()
         self._threadExecutor = None
         self._threadExecutable = False
         self.repeat = 0
@@ -44,25 +47,41 @@ class MusicTerminal(InputParser):
         return futureObj
 
     @InputParser.command
-    def add(self):
+    def add(self, *arg):
         """ shortcut decorator that invokes
         1. findSong()
         2. _addsong()
 
         args: possible a thread executor
         """
-        path, songs = findSong()
-        if songs.__sizeof__() == 40:
-            return print("Not found, Try Harder!!")
+        if len(arg) >= 1 and arg[0] != 'many':
+            raise Exception('Usage : /add or /add many')
         
-        print(' Song Added')
-        return self._addsong(path, songs, False)
+        if arg:
+            # find multiple songs
+            result = ''
+            getdict = findmany(',')
+            for path, songs in getdict.items():
+                if path is not None and songs.__sizeof__() != 40:
+                    temp = self._addsong(path, songs, False)
+                    if temp is not None:
+                        result = temp
+                    del temp
+            
+            print('Songs Added')
+            return result
+
+        else:
+            path, songs = findone()
+            print('Song Added')
+            return self._addsong(path, songs, False)
 
     def _addsong(self, path: str, songs: list, database: bool):
         """ executor should be an obj of 
         concurrent.futures.ThreadPoolExecutor """
 
         if database:
+            self.playlistpos = len(songs)
             for song in songs:
                 self._player.playlist_append(song[0])
                 self.playlist0.append([song[0]])
@@ -103,8 +122,8 @@ class MusicTerminal(InputParser):
 
                 self.playing_msg = "running"
                 self.running = not self._player.idle_active
-                print(self.running, self._player.idle_active)
-                print(f"\n == {self.playing_msg} ==\n")
+                #print(self.running, self._player.idle_active) 
+                #print(f"\n == {self.playing_msg} ==\n")
             else:
                 self.playing_msg = "stopped"
                 self.running = self._player.idle_active
@@ -121,9 +140,7 @@ class MusicTerminal(InputParser):
                 totaltime = (
                     self._player.time_remaining + self._player.time_pos
                 ) - self._player.time_pos
-                print("wait until", totaltime)
-                exit.wait(timeout=totaltime)  # have to make it wait
-                print("loop stopped")
+                self._threadStopper.wait(timeout=totaltime)  # have to make it wait
                 if self.repeat > 0 and self.running:
                     self.repeat -= 1
                 if self.repeat == 0 and self.running:
@@ -133,13 +150,16 @@ class MusicTerminal(InputParser):
                         return err.args[0]
                 if self._player.playlist_count > 0:
                     self._player.wait_until_playing()  # wait until new song starts
+            else:
+                break
 
-        print("here")
         self.stop(False)  # do not terminate
         return True
 
     @InputParser.command
     def current(self, show=True):
+        if not self._player.playlist:
+            return print('Playlist Empty')
         songname = filter(lambda x: "playing" in x, self._player.playlist)
         songname = os.path.basename(list(songname)[0]["filename"])
         return songname if not show else print(songname)
@@ -159,10 +179,13 @@ class MusicTerminal(InputParser):
     @InputParser.command
     def pause(self):
         # Not in idle state
+        if not self.running:
+            raise Exception('Player is Already Stopped/Paused')
         self._player.pause = True
         self.running = self._player.idle_active
         self.playing_msg = "paused"
-        exit.set()
+        if not self._threadStopper.is_set():
+            self._threadStopper.set()
 
     @InputParser.command
     def repeat(self, others):
@@ -170,6 +193,10 @@ class MusicTerminal(InputParser):
         # Put the current song in repeat (Under-working)
         # no : normal playback
         # inf : infinite
+
+        if not self._threadExecutable:
+            raise Exception('Nothing to Repeat')
+
         params = ["inf", True, False, "no", "yes"]
         if not (others in params):
             if str(others) not in "0123456789":
@@ -183,28 +210,49 @@ class MusicTerminal(InputParser):
     @InputParser.command
     def resume(self):
         # Not in idle state
-        self._player.pause = False
-        self.running = not self._player.idle_active
-        self.playing_msg = "running"
-        exit.clear()
+        # while playing a song 
+        if self.running and self._threadExecutable:
+            raise Exception('Nothing on paused rn')
+        # while player is terminated
+        elif not self.running and not self._threadExecutable:
+            raise Exception('Player is already stopped')
+        # while player is paused (running = False, threadExecutable = True)
+        else:
+            self._player.pause = False
+            self.running = not self._player.idle_active
+            self.playing_msg = "running"
+            self._threadStopper.clear()
 
+    ##### FIX THIS [TERMINATE THE OBJECT AND DON'T RESUE IT #####
     @InputParser.command
     def stop(self, terminate=False):
-        # put the player in IDLE state
-        # stop(): Clears the playlist
         # Not working : keep_playlist
+        
+        terminate = bool(terminate)
         self._player.stop()
+        if self.running and not self._threadStopper.is_set():
+            self._threadStopper.set() # set the flag and ends the func. has event.wait()
+        self._player.pause = False
         self.running = False
         self.playing_msg = "stopped"
         self._threadExecutable = False
-        if terminate:
-            self._player.terminate()  # stop the thread. Destroys the mpv object
-        exit.set() # set the flag
-        exit.clear() # remove the flag
+        if isinstance(terminate, bool) and terminate is True:
+            if self._threadStopper.is_set():
+                self._threadStopper.clear()
+            # stop the thread and destroys the mpv object
+            self._player.quit()
+            self._player.wait_for_shutdown()
+            self._player.terminate()
+
+            # free all the resources (thread function) executor has executed..
+            self._threadExecutor.shutdown(wait=True) # prevents executor to accepts new threads
+ 
+        if self._threadStopper.is_set():
+            self._threadStopper.clear() # remove the flag [for re-use]
 
     @InputParser.command
     def queue(self):
-        if self.playing_msg == 'stopped':
+        if not self.running and not self._threadExecutable:
             return print("Playlist empty")
         print('\n [')
         current = self.current(show=False)
