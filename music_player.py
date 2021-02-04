8cf8ac560a23affa0df2af8265c4c7a6e6f49b41
Himanshu Bhatnagar <33115688+Himan10@users.noreply.github.com>
2021-02-04 23:41:33 +0530

    Fixed future running problem after stop()
    Problem: future instance was running if the user makes a call to stop() when the music player is in the PAUSED state.
    Fix: in the previous code structure, I was using `wait_until_playing()` which waits till the song starts but now I added a lambda function that makes a call to `wait_until_playing()` if the player is running, and if it's not running, then acquire the semaphore. In this way, if the player is on pause then the thread has to wait until another release a semaphore...
    Now, if a call is made to `stop()` then the semaphore will get released...
    Right now, I am using an object attribute to store Semaphore() instance.. will do smth later.. 
    (sorry for  the random silly changes)

--- music_player.py
+++ music_player.py
@@ -1,11 +1,15 @@
-import mpv
+from mpv import MPV
 from threading import Event
+from threading import Thread
+from threading import enumerate
+from threading import current_thread
+from threading import Semaphore
 from SongPathFinder import os
 from SongPathFinder import findone, findmany
 from connectToSql import SongDatabase
 from command_handler import Command
 from command_handler import InputParser
-from concurrent.futures import Executor # for executor type checking
+from concurrent.futures import Executor  # for executor type checking
 from concurrent.futures import ThreadPoolExecutor
 
 
@@ -14,19 +18,22 @@ class MusicTerminal(InputParser):
         return self
 
     def __exit__(self, type, value, traceback):
-        self.stop(True)
+        self.stop("true")
         return isinstance(value, TypeError)
 
-    def __init__(self, video: bool, prefix: str, ytdl=True, logging=False, event_thread=True):
+    def __init__(
+        self, video: bool, prefix: str, ytdl=True, logging=False, event_thread=True
+    ):
         super().__init__(prefix)
-        self._player = mpv.MPV()  # returns a proxy object. (dont touch this)
+        self._player = MPV()  # returns a proxy object. (dont touch this)
         self.playlist0 = []  # temp. (provide to SQL)
-        self.playlistpos = 0 # orignal playlist position 
+        self.playlistpos = 0  # orignal playlist position
         self._player.video = video
         self._player.ytdl = ytdl
         self.playing_msg = "stopped"
         self.logging = logging
         self.running = False
+        self._sema = Semaphore(0)
         self._threadStopper = Event()
         self._threadExecutor = None
         self._threadExecutable = False
@@ -39,7 +46,7 @@ class MusicTerminal(InputParser):
         """ shortcut decorator that invokes
         1. _addsong()
         Called only at the startup
-        """        
+        """
         futureObj = self._addsong(None, song_playlist, True)
         return futureObj
 
@@ -51,26 +58,26 @@ class MusicTerminal(InputParser):
 
         args: possible a thread executor
         """
-        if len(arg) >= 1 and arg[0] != 'many':
-            raise Exception('Usage : /add or /add many')
-        
+        if len(arg) >= 1 and arg[0] != "many":
+            raise Exception("Usage : /add or /add many")
+
         if arg:
             # find multiple songs
-            result = ''
-            getdict = findmany(',')
+            result = ""
+            getdict = findmany(",")
             for path, songs in getdict.items():
                 if path is not None and songs.__sizeof__() != 40:
                     temp = self._addsong(path, songs, False)
                     if temp is not None:
                         result = temp
                     del temp
-            
-            print('Songs Added')
+
+            print("Songs Added")
             return result
 
         else:
             path, songs = findone()
-            print('Song Added')
+            print("Song Added")
             return self._addsong(path, songs, False)
 
     def _addsong(self, path: str, songs: list, database: bool):
@@ -86,7 +93,7 @@ class MusicTerminal(InputParser):
         else:
             # provide values to SQLite DB. [[], []]
             for song in songs:
-                
+
                 if path is None and song not in self._player.playlist_filenames:
                     self._player.playlist_append(song)
                     self.playlist0.append([song])
@@ -101,7 +108,7 @@ class MusicTerminal(InputParser):
             if self._threadExecutable:
                 return None
             else:
-                future1 = self._play() # Call it only once or if adding a song
+                future1 = self._play()  # Call it only once or if adding a song
                 return future1  # contains result of another thread
 
     def _play(self):
@@ -119,8 +126,8 @@ class MusicTerminal(InputParser):
 
                 self.playing_msg = "running"
                 self.running = not self._player.idle_active
-                #print(self.running, self._player.idle_active) 
-                #print(f"\n == {self.playing_msg} ==\n")
+                # print(self.running, self._player.idle_active)
+                # print(f"\n == {self.playing_msg} ==\n")
             else:
                 self.playing_msg = "stopped"
                 self.running = self._player.idle_active
@@ -130,6 +137,9 @@ class MusicTerminal(InputParser):
     def _track_song(self):  # Because we want real timestamp updates, not only once.
         # bug : continues while the song is on PAUSE (Solved)
 
+        wait_until_condition = (
+            lambda cond: wait_until_playing() if cond else self._sema.acquire()
+        )
         while self._player.playlist_count > 0:
 
             if self._threadExecutable:
@@ -144,19 +154,19 @@ class MusicTerminal(InputParser):
                     try:
                         self.remove(0, True)  # Delete the song from both queues
                     except Exception as err:
-                        return err.args[0]
+                        return err.args[0], "Here"
                 if self._player.playlist_count > 0:
-                    self._player.wait_until_playing()  # wait until new song starts
+                    wait_until_condition(self.running)
             else:
                 break
 
-        self.stop(False)  # do not terminate
+        self.stop("false")  # do not terminate
         return True
 
     @InputParser.command
     def current(self, show=True):
         if not self._player.playlist:
-            return print('Playlist Empty')
+            return print("Playlist Empty")
         songname = filter(lambda x: "playing" in x, self._player.playlist)
         songname = os.path.basename(list(songname)[0]["filename"])
         return songname if not show else print(songname)
@@ -177,7 +187,7 @@ class MusicTerminal(InputParser):
     def pause(self):
         # Not in idle state
         if not self.running:
-            raise Exception('Player is Already Stopped/Paused')
+            raise Exception("Player is Already Stopped/Paused")
         self._player.pause = True
         self.running = self._player.idle_active
         self.playing_msg = "paused"
@@ -192,7 +202,7 @@ class MusicTerminal(InputParser):
         # inf : infinite
 
         if not self._threadExecutable:
-            raise Exception('Nothing to Repeat')
+            raise Exception("Nothing to Repeat")
 
         params = ["inf", True, False, "no", "yes"]
         if not (others in params):
@@ -207,28 +217,30 @@ class MusicTerminal(InputParser):
     @InputParser.command
     def resume(self):
         # Not in idle state
-        # while playing a song 
+        # while playing a song
         if self.running and self._threadExecutable:
-            raise Exception('Nothing on paused rn')
+            raise Exception("Nothing on paused rn")
         # while player is terminated
         elif not self.running and not self._threadExecutable:
-            raise Exception('Player is already stopped')
+            raise Exception("Player is already stopped")
         # while player is paused (running = False, threadExecutable = True)
         else:
             self._player.pause = False
             self.running = not self._player.idle_active
             self.playing_msg = "running"
             self._threadStopper.clear()
+            if self._sema._value < 1:
+                self._sema.release()
 
-    ##### FIX THIS [TERMINATE THE OBJECT AND DON'T RESUE IT #####
+    # FIX THIS TERMINATE THE OBJECT AND DON'T RESUE IT
     @InputParser.command
-    def stop(self, terminate=False):
+    def stop(self, terminate="False"):
         # Not working : keep_playlist
-        
-        terminate = True if terminate == "True" else False
+        # bool("False") returns True
+        terminate = True if terminate.lower() == "true" else False
         self._player.stop()
         if self.running and not self._threadStopper.is_set():
-            self._threadStopper.set() # set the flag and ends the func. has event.wait()
+            self._threadStopper.set()  # set the flag and ends the func. has event.wait()
         self._player.pause = False
         self.running = False
         self.playing_msg = "stopped"
@@ -242,24 +254,29 @@ class MusicTerminal(InputParser):
             self._player.terminate()
 
             # free all the resources (thread function) executor has executed..
-            self._threadExecutor.shutdown(wait=True) # prevents executor to accepts new threads
- 
+            self._threadExecutor.shutdown(
+                wait=True
+            )  # prevents executor to accepts new threads
+
         if self._threadStopper.is_set():
-            self._threadStopper.clear() # remove the flag [for re-use]
+            self._threadStopper.clear()  # remove the flag [for re-use]
+
+        if self._sema._value < 1:
+            self._sema.release()
 
     @InputParser.command
     def queue(self):
         if not self.running and not self._threadExecutable:
             return print("Playlist empty")
-        print('\n [')
+        print("\n [")
         current = self.current(show=False)
         for songname in self._player.playlist_filenames:
             songname = os.path.basename(songname)
             if songname == current:
-                print('   [Current]    ', current)
+                print("   [Current]    ", current)
             else:
-                print('\t\t', songname)
-        print(' ]\n')
+                print("\t\t", songname)
+        print(" ]\n")
 
     @InputParser.command
     def playlist(self, username: str, options):
